d4de6e58e9876f44aab1defab42217a6680d7b06
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-03-21 13:59:26 +0530

    Add files via upload

--- Switch/main.dart
+++ Switch/main.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  createState() => MyAppState();
+}
+
+
+class MyAppState extends State<MyApp> {
+  bool isSwitched = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Switch Check',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Switch Check"),
+    ),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Switch(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+              activeTrackColor: Colors.lightGreenAccent,
+              activeColor: Colors.green,
+            )
+            ]
+          ),
+        ),
+      )
+    );
+  }
+}
