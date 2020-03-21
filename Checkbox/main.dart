d4c996258d517053a13fdf054a3259b81a681b87
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-03-21 13:57:50 +0530

    Add files via upload

--- Checkbox/main.dart
+++ Checkbox/main.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  createState() => MyAppState();
+}
+
+
+class MyAppState extends State<MyApp> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Checkbox',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Checkbox"),
+          ),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked = value;
+                      });
+                    },
+                  ),
+                ]
+            ),
+          ),
+        )
+    );
+  }
+}
