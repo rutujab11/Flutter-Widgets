c033d16a85aaa34a895fc77b3ed1c861f9170355
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-03-21 14:05:00 +0530

    Add files via upload

--- Slider/main.dart
+++ Slider/main.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
 
+void main(){
+  runApp(MaterialApp(
+    home: MainActivity(),
+  ));
+}
+
+class MainActivity extends StatefulWidget{
+  @override
+  _MainActivityState createState() => _MainActivityState();
+}
+
+class _MainActivityState extends State<MainActivity>{
+  double _value= 0.0;
+
+  onChanged(double value){
+    setState(() {
+      _value = value;
+    });
+  }
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sliders'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Slider(
+              value: _value,
+                onChanged: onChanged),
+            )
+          ],
+          ),
+      );
+  }
+}
