382bb4c6b7ec136b3aec5f722bb5e83dc5939278
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-03-21 14:21:53 +0530

    Add files via upload
    Retrieves information from given site in JSON format using HTTP requests and response

--- HTTP requests/main.dart
+++ HTTP requests/main.dart
@@ -1,1 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+void main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+
+  Future<String> getData() async{
+    http.Response response= await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+        headers: {
+            "Accept": "application/json"
+      }
+    );
+
+    print(response.body);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Get Data"),
+          onPressed: getData,
+        ),
+      ),
+    );
+  }
+}
