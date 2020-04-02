3aff625c808c2989a775a2e75c8595a6911f356c
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-04-02 16:22:34 +0530

    Add files via upload

---
+++ basics/basic form/Constants.dart
@@ -0,0 +1,14 @@
+class Constants{
+  static const String Account = 'Account';
+  static const String Settings = 'Settings';
+  static const String Help = 'Help';
+  static const String SignOut = 'SignOut';
+
+  static const List<String> choices= <String>[
+    Account,
+    Settings,
+    Help,
+    SignOut
+  ];
+
+}
