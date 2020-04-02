bf03dc6ec8cc6ead26c3e3ed097493d0b70ff328
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-04-02 16:10:27 +0530

    Add files via upload
    Basic Details(card)

--- basics/main.dart
+++ basics/main.dart
@@ -1,98 +1,183 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: BasicInfo(),
 ));
 
-class Home extends StatelessWidget {
+class BasicInfo extends StatefulWidget {
+  @override
+  _BasicInfoState createState() => _BasicInfoState();
+}
+class _BasicInfoState extends State<BasicInfo> {
+
+  int bookCount = 0;
+  DateTime _date = new DateTime.now();
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: new DateTime(1947),
+        lastDate: new DateTime(2021)
+    );
+
+
+    if(picked != null && picked != _date) {
+      print('Date Selected: ${_date.toString()}');
+      setState(() {
+        _date = picked;
+      });
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Trial'),
+        title: Text('User Info'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Text('One'),
-            color: Colors.amberAccent
-          ),
-          Container(
-              padding: EdgeInsets.all(20.0),
-              child: Text('Two'),
-              color: Colors.indigoAccent
-          ),
-          Container(
-              padding: EdgeInsets.all(20.0),
-              child: Text('Three'),
-              color: Colors.deepOrangeAccent
-          ),
-        ],
-      ),
-      /*body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Text('Hello World'),
-          RaisedButton(
-            onPressed: () {},
-            color: Colors.grey,
-            child: Text('Click')
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.indigoAccent,
-            child: Text('inside the container'),
-          )
-        ],
-      ),*/
-      /*body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('Capture');
-          },
-          icon: Icon(
-            Icons.photo_camera,
-          ),
-        ),*/
-        /*icon button
-        child: RaisedButton.icon(
-          onPressed: () {},
-          icon: Icon(
-            Icons.cloud,
-            color: Colors.white,
-          ),
-          label: Text('Click') ,
-          color: Colors.blueGrey,
-        ),*/
-        /*icons
-        child: Icon(
-          Icons.battery_charging_full,
-          color: Colors.green[700],
-          size: 50.0,
-        ),*/
-        /*for text and styling
-        child: Text(
-            'Hello World',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),*/
-        /*for image
-        child: Image(
-          image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS3j5lFvBNxsZ7iegdavl1kkW0BiMlfpZUPklfBKUubYxkAhg6o'),
-        ),*/
-
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Text('Click'),
-        backgroundColor: Colors.red[600],
+          onPressed: () {
+            setState(() {
+              bookCount += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor:  Colors.grey[800],
+      ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/image.png'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(
+                height: 40.0,
+                color: Colors.grey[800],
+              ),
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Rutuja Bhandigani',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 1.5,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 22.0),
+              Text(
+                'NUMBER OF BOOKS ISSUED',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '$bookCount',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 1.5,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 22.0),
+              Text(
+                'DATE OF BIRTH',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {_selectDate(context);},
+                    icon: Icon(
+                      Icons.calendar_today,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                      '${_date.toString()}',
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 1.5,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+              /*SizedBox(height: 30.0),
+              Text(
+                'PHONE NO.',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '8769054645',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 1.5,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),*/
+              SizedBox(height: 22.0),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'rutuja.b@gmail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
       ),
     );
   }
